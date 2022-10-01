659d3ded1219585a32eee7e05b4307e2c6156623
Taeangel <wngks2050@gmail.com>
2022-10-02 02:12:29 +0900

    file: 파일정리

---
+++ Swipe/Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by song on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
