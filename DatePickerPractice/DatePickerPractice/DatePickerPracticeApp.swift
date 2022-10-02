1ba93b6d5bc648db3d5591c9dafa7d74674b10f6
Taeangel <wngks2050@gmail.com>
2022-10-03 00:23:58 +0900

    DatePickerPractice 파일 생성

---
+++ DatePickerPractice/DatePickerPractice/DatePickerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerPracticeApp.swift
+//  DatePickerPractice
+//
+//  Created by song on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
