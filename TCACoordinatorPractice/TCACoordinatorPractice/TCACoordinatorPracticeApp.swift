b2c04c9a1b37cb8536d288ef49c13b040ac6d7a9
Taeangel <wngks2050@gmail.com>
2022-10-03 00:25:17 +0900

    TCACoordinatorPractice 파일생성

---
+++ TCACoordinatorPractice/TCACoordinatorPractice/TCACoordinatorPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACoordinatorPracticeApp.swift
+//  TCACoordinatorPractice
+//
+//  Created by song on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct TCACoordinatorPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
