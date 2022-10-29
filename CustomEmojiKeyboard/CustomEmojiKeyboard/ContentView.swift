f5543380c68549abf719fa4ebf4e75dd26dd01e8
Taeangel <wngks2050@gmail.com>
2022-10-29 20:08:38 +0900

    refactor: EmojiView 생성

--- CustomEmojiKeyboard/CustomEmojiKeyboard/ContentView.swift
+++ CustomEmojiKeyboard/CustomEmojiKeyboard/ContentView.swift
@@ -18,39 +18,20 @@ struct ContentView: View {
         VStack {
           HStack(spacing: 15) {
             TextField("Message", text: self.$txt)
-            
-            Button(action: {
-              
-              self.show.toggle()
-              
-            }, label: {
-              Image(systemName: "smiley")
-                .foregroundColor(Color.black.opacity(0.5))
-            })
           }
           .padding(12)
           .background(Color.white)
           .clipShape(Capsule() )
+          
+          EmojiView(txt: $txt)
         }
       }
-      EmojiView(show: $show, txt: $txt)
-        .offset(y: self.show ?  (UIApplication.shared.windows.first?.safeAreaInsets.bottom)! : UIScreen.main.bounds.height)
     }
     .background(Color("Color").edgesIgnoringSafeArea(.all))
-    .animation(.default)
-    
-  }
-}
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
   }
 }
 
 struct EmojiView: View {
-  
-  @Binding var show: Bool
   @Binding var txt: String
   
   var body: some View {
@@ -64,14 +45,12 @@ struct EmojiView: View {
                   guard let unicode = UnicodeScalar(j) else {
                     return
                   }
-                  
-                  self.txt += String(unicode)
-                  
+                  self.txt = String(unicode)
                 },
                        label: {
                   if UnicodeScalar(j)?.properties.isEmoji != nil, let unicode = UnicodeScalar(j) {
                     Text(String(unicode))
-                      .font(.system(size: 30))
+                      .font(.system(size: 20))
                   } else {
                     Text("")
                   }
@@ -82,21 +61,9 @@ struct EmojiView: View {
         }
         .padding(.top)
       }
-      .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/3)
-      .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+      .frame(width: UIScreen.main.bounds.width / 4 * 3, height: UIScreen.main.bounds.height/3)
       .background(Color.white)
       .cornerRadius(25)
-
-      Button(action: {
-       
-        self.show.toggle()
-      },
-             label: {
-        
-        Image(systemName: "xmark")
-          .foregroundColor(.black)
-      })
-      .padding()
     }
   }
   
