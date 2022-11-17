683ff03f461d1317465696a2e50af38e3bfc7257
Taeangel <wngks2050@gmail.com>
2022-11-18 03:07:15 +0900

    refactor: CoinApp Setting

---
+++ CoinApp/CoinApp/CoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAppApp.swift
+//  CoinApp
+//
+//  Created by song on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct CoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- SwiftUICryptoApp/SwiftUICryptoApp/Extension/Color.swift
+++ SwiftUICryptoApp/SwiftUICryptoApp/Extension/Color.swift
@@ -13,7 +13,6 @@ extension Color {
   
 }
 
-
 struct ColorTheme {
   
   let accent = Color("AccentColor")
